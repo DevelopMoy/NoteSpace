9796f657e8893de2f9eca6f17bf691708d98eccc
Moises <fabmoy1866@gmail.com>
2020-05-10 20:42:59 -0500

    Initial settings done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    VentanaPrincipal v1=new VentanaPrincipal();
+	    MainWindow v1=new MainWindow();
     }
 }
--- src/com/company/MainWindow.java
+++ src/com/company/MainWindow.java
@@ -2,12 +2,13 @@ package com.company;
 
 import javax.swing.*;
 
-public class VentanaPrincipal extends JFrame {
-    public VentanaPrincipal (){
+public class MainWindow extends JFrame {
+    public MainWindow (){
         super.setSize(1024,768);
         super.setLocationRelativeTo(null);
+        super.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
         super.setVisible(true);
     }
-    
+
 }
