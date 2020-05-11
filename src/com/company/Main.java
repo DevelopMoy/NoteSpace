433d1dee2b79bdf23461375229832c2f5557334f
Moises <fabmoy1866@gmail.com>
2020-05-10 20:34:16 -0500

    INITIAL COMMIT

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    VentanaPrincipal v1=new VentanaPrincipal();
+    }
+}
---
+++ src/com/company/VentanaPrincipal.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import javax.swing.*;
+
+public class VentanaPrincipal extends JFrame {
+    public VentanaPrincipal (){
+        super.setSize(1024,768);
+        super.setLocationRelativeTo(null);
+
+        super.setVisible(true);
+    }
+    
+}
