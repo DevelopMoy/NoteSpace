00f66c3ab06890f0a767155f9d52bf904ee698c4
Moises <fabmoy1866@gmail.com>
2020-05-12 10:42:12 -0500

    Save Function added in the actionPerformed method of the button

--- src/com/company/MainPanel.java
+++ src/com/company/MainPanel.java
@@ -1,9 +1,17 @@
 package com.company;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MainPanel extends JPanel {
+    private Component thisComp =this;
     private JTextArea mainArea=new JTextArea();
     private JScrollPane scrollTextArea=new JScrollPane (mainArea);
     private ToolsBar mainToolsBar =new ToolsBar();
@@ -22,9 +30,53 @@ public class MainPanel extends JPanel {
     private class ToolsBar extends JPanel{
         private JButton saveButton =new JButton("Save to .txt");
         private JButton clearButton = new JButton("Clear All");
+
         ToolsBar (){
             add (saveButton);
             add(clearButton);
+            addFunctionToButtons();
+        }
+
+        private void addFunctionToButtons(){
+            clearButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    mainArea.setText("");
+                }
+            });
+
+            saveButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    JFileChooser select=new JFileChooser();
+                    FileWriter fw;
+                    File writteFile;
+                    BufferedWriter bw;
+                    try {
+                        int returnValue = select.showSaveDialog(thisComp);
+                        if (returnValue==JFileChooser.APPROVE_OPTION){
+                            System.out.println("YOU SELECTED: "+select.getCurrentDirectory().toString()+" FILE NAME: "+select.getSelectedFile().getName());
+                            writteFile=new File(select.getCurrentDirectory()+"/"+select.getSelectedFile().getName());
+                            if (!writteFile.exists()){
+                                writteFile.createNewFile();
+                            }
+                            fw=new FileWriter(writteFile);
+                            bw=new BufferedWriter(fw);
+                            bw.write(mainArea.getText());
+                            if (bw!=null){
+                                bw.close();
+                            }
+                        }
+                    }catch (HeadlessException e){
+                        System.out.println("INPUT/OUTPUT ERROR: "+e.getMessage());
+                    }catch (NullPointerException e){
+                        System.out.println("NULL POINTER ERROR"+e.getMessage());
+                    }catch (Exception e){
+                        System.out.println("I/O ERROR "+e.getMessage());
+                    }
+
+                }
+            });
         }
     }
 }
