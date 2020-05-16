dfe15fbeaa44b2b948100fb8bd3b8051505864c3
Moises <fabmoy1866@gmail.com>
2020-05-15 23:51:50 -0500

    Added open function which can read .txt files including their content to the main area, added validation of recent changes to the main area in order to know if you will loose data if you open a new file. The save function that were previously on the actionPerformed method was changed to a function in the ToolsBar class

--- src/com/company/MainPanel.java
+++ src/com/company/MainPanel.java
@@ -1,23 +1,24 @@
 package com.company;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class MainPanel extends JPanel {
     private Component thisComp =this;
     private JTextArea mainArea=new JTextArea();
     private JScrollPane scrollTextArea=new JScrollPane (mainArea);
     private ToolsBar mainToolsBar =new ToolsBar();
+    private boolean saveState=false;
 
     public MainPanel (){
         mainArea.setLineWrap(true);
+        mainArea.getDocument().addDocumentListener(new DocumentList());
         layoutConfig();
     }
 
@@ -27,14 +28,92 @@ public class MainPanel extends JPanel {
         add (scrollTextArea,BorderLayout.CENTER);
     }
 
+    private class DocumentList implements DocumentListener {
+
+        @Override
+        public void insertUpdate(DocumentEvent documentEvent) {
+            saveState=false;
+        }
+
+        @Override
+        public void removeUpdate(DocumentEvent documentEvent) {
+            saveState=false;
+        }
+
+        @Override
+        public void changedUpdate(DocumentEvent documentEvent) {
+            saveState=false;
+        }
+    }
+
     private class ToolsBar extends JPanel{
         private JButton saveButton =new JButton("Save to .txt");
         private JButton clearButton = new JButton("Clear All");
+        private JButton openButton = new JButton("Open");
 
         ToolsBar (){
+            add (openButton);
             add (saveButton);
             add(clearButton);
             addFunctionToButtons();
+        }
+
+        private void saveFunction (){
+            JFileChooser select=new JFileChooser();
+            FileWriter fw;
+            File writteFile;
+            BufferedWriter bw;
+            try {
+                int returnValue = select.showSaveDialog(thisComp);
+                if (returnValue==JFileChooser.APPROVE_OPTION){
+                    System.out.println("YOU SELECTED: "+select.getCurrentDirectory().toString()+" FILE NAME: "+select.getSelectedFile().getName());
+                    writteFile=new File(select.getCurrentDirectory()+"/"+select.getSelectedFile().getName());
+                    if (!writteFile.exists()){
+                        writteFile.createNewFile();
+                    }
+                    fw=new FileWriter(writteFile);
+                    bw=new BufferedWriter(fw);
+                    bw.write(mainArea.getText());
+                    saveState=true;
+                    if (bw!=null){
+                        bw.close();
+                    }
+                }
+            }catch (HeadlessException e){
+                System.out.println("INPUT/OUTPUT ERROR: "+e.getMessage());
+            }catch (NullPointerException e){
+                System.out.println("NULL POINTER ERROR"+e.getMessage());
+            }catch (Exception e){
+                System.out.println("I/O ERROR "+e.getMessage());
+            }
+        }
+
+        private void openFunction (){
+
+            JFileChooser choose=new JFileChooser();
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("TXT FILE","txt");
+            choose.setFileFilter(filter);
+            int returnVal =choose.showOpenDialog(thisComp);
+            if (returnVal==JFileChooser.APPROVE_OPTION){
+                String data;
+                String path=choose.getCurrentDirectory()+"/"+choose.getSelectedFile().getName();
+                try  {
+                    FileReader reader =new FileReader(new File(choose.getCurrentDirectory()+"/"+choose.getSelectedFile().getName()));
+                    BufferedReader buffReader = new BufferedReader(reader);
+                    do{
+                        data=buffReader.readLine();
+                        if (data!=null){
+                            mainArea.append(data);
+                        }
+                    }while (data!=null);
+                }catch (NullPointerException e){
+                    System.out.println(e.getMessage());
+                }catch (FileNotFoundException e){
+                    JOptionPane.showMessageDialog(thisComp,"ERROR, FILE NOT FOUND "+e.getMessage());
+                }catch (IOException e){
+                    JOptionPane.showMessageDialog(thisComp,"ERROR, INPUT / OUTPUT ERROR "+e.getMessage());
+                }
+            }
         }
 
         private void addFunctionToButtons(){
@@ -45,36 +124,39 @@ public class MainPanel extends JPanel {
                 }
             });
 
+            openButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    int op;
+                    boolean enter=true;
+                    if ((!(mainArea.getText().compareTo("")==0))&&saveState==false){
+                        op=JOptionPane.showConfirmDialog(thisComp,"DO YOU WANT TO SAVE CHANGES FOR THIS DOCUMENT?");
+                        if (op==JOptionPane.YES_OPTION){
+                            System.out.println("yes");
+                            saveFunction();
+                        }else {
+                            if (op==JOptionPane.NO_OPTION){
+                                System.out.println("NO");
+                                mainArea.setText("");
+                            }else {
+                                if (op==JOptionPane.CANCEL_OPTION){
+                                    System.out.println("CANCEL");
+                                    enter=false;
+                                }
+                            }
+                        }
+                    }
+                    if (enter){
+                        openFunction();
+                    }
+                }
+            });
+
+
             saveButton.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent actionEvent) {
-                    JFileChooser select=new JFileChooser();
-                    FileWriter fw;
-                    File writteFile;
-                    BufferedWriter bw;
-                    try {
-                        int returnValue = select.showSaveDialog(thisComp);
-                        if (returnValue==JFileChooser.APPROVE_OPTION){
-                            System.out.println("YOU SELECTED: "+select.getCurrentDirectory().toString()+" FILE NAME: "+select.getSelectedFile().getName());
-                            writteFile=new File(select.getCurrentDirectory()+"/"+select.getSelectedFile().getName());
-                            if (!writteFile.exists()){
-                                writteFile.createNewFile();
-                            }
-                            fw=new FileWriter(writteFile);
-                            bw=new BufferedWriter(fw);
-                            bw.write(mainArea.getText());
-                            if (bw!=null){
-                                bw.close();
-                            }
-                        }
-                    }catch (HeadlessException e){
-                        System.out.println("INPUT/OUTPUT ERROR: "+e.getMessage());
-                    }catch (NullPointerException e){
-                        System.out.println("NULL POINTER ERROR"+e.getMessage());
-                    }catch (Exception e){
-                        System.out.println("I/O ERROR "+e.getMessage());
-                    }
-
+                    saveFunction();
                 }
             });
         }
